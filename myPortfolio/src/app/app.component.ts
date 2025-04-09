2da59b80b6f9e06d6db1db1e0ef5fe97ac5feaae
iliassid <enaailiass@gmail.com>
2025-04-09 15:47:07 +0100

    done with home page

--- myPortfolio/src/app/app.component.ts
+++ myPortfolio/src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent],
   template: `
     <app-header/>
     <app-home/>
+    <app-footer/>
   `,
   styles: [],
 })
