1d98f9e63f3d4b6bffe512c86a7d6a34cd50fa95
iliassid <enaailiass@gmail.com>
2025-04-11 17:26:39 +0100

    finished the contact and cv component

--- myPortfolio/src/app/app.routes.ts
+++ myPortfolio/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectComponent } from './components/project/project.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { CvComponent } from './components/cv/cv.component';
 
 export const routes: Routes = [
     {path: '', pathMatch:'full', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'project', component: ProjectComponent},
+    {path: 'cv', component: CvComponent},
     {path: 'contact', component: ContactComponent}
 ];
